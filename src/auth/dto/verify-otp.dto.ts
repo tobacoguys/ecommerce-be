c14a7ea58bc75018ec39292616cc4a9c73e43f97
tobacoguys <dungchelsea1723@gmail.com>
2025-02-24 13:34:20 +0700

    update verify otp dto

--- src/auth/dto/verify-otp.dto.ts
+++ src/auth/dto/verify-otp.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class VerifyOtpDto {
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    @MaxLength(6)
     otp: string;
 }
