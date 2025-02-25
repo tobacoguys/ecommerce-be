9b5c7fd02cb9cb257896b52612b69f2299116591
tobacoguys <dungchelsea1723@gmail.com>
2025-02-25 23:34:33 +0700

    API update profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+import { Request as ExpressRequest } from 'express';
+
+interface RequestWithUser extends ExpressRequest {
+    user: {
+        id: string;
+        // add other user properties if needed
+    };
+}
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Patch('/profile')
+    @UseGuards(JwtAuthGuard)
+    async updateProfile(@Request() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
+        const userId = req.user.id;
+        const updatedUser = await this.userService.updateProfile(
+            userId,
+            updateUserDto,
+        );
+        return { message: 'Profile updated successfully', data: updatedUser };
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import User from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {}
+
+    async updateProfile( userId: string, updateUserDto: UpdateUserDto): Promise<User> {
+        const user = await this.userRepository.findOne({ where: { id: userId } });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        const { username, bio, birthday, phone, address } = updateUserDto;
+        if (username) user.username = username;
+        if (bio) user.bio = bio;
+        if (birthday) user.birthday = birthday;
+        if (phone) user.phone = phone;
+        if (address) user.address = address;
+
+        await this.userRepository.save(user);
+        return user;
+    }
+}
