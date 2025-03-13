09c0b04eb27f097f8b1a874f7a88b878a8f99f37
tobacoguys <dungchelsea1723@gmail.com>
2025-03-13 21:57:43 +0700

    API update cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { AddToCartDto } from './dto/add-to-cart.dto';
 import User from 'src/user/entity/user.entity';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { UpdateCartDto } from './dto/update-cart.dto';
 
 @Controller('cart')
 export class CartController {
@@ -18,5 +19,11 @@ export class CartController {
     @UseGuards(JwtAuthGuard)
     async getCart(@Req() req: { user: User }) {
       return this.cartService.getCart(req.user.id);
+    }
+
+    @Patch('/update/:cartId')
+    @UseGuards(JwtAuthGuard)
+    async updateCart(@Param('cartId') cartId: string, @Body() updateCartDto: UpdateCartDto) {
+        return this.cartService.updateCart(cartId, updateCartDto);
     }
 }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -4,6 +4,7 @@ import { Cart } from './cart.entity';
 import { Repository } from 'typeorm';
 import { AddToCartDto } from './dto/add-to-cart.dto';
 import Product from 'src/product/product.entity';
+import { UpdateCartDto } from './dto/update-cart.dto';
 
 @Injectable()
 export class CartService {
@@ -56,4 +57,29 @@ export class CartService {
     async getCart(userId: string) {
         return this.cartRepository.find({ where: { user: { id: userId } }, relations: ['product'] });
       }
+
+    async updateCart(cartId: string, updateCartDto: UpdateCartDto): Promise<{ cart: Cart[], totalAmount: number }> {
+        const cartItem = await this.cartRepository.findOne({ 
+            where: { id: cartId },
+            relations: ['product', 'user'] 
+        });
+
+        if (!cartItem) {
+            throw new NotFoundException('Cart item not found');
+        }
+
+        cartItem.quantity = updateCartDto.quantity;
+        cartItem.totalPrice = cartItem.quantity * cartItem.product.price;
+
+        await this.cartRepository.save(cartItem);
+
+        const cart = await this.cartRepository.find({
+            where: { user: { id: cartItem.user.id } },
+            relations: ['product']
+        });
+
+        const totalAmount = cart.reduce((sum, item) => sum + Number(item.totalPrice), 0);
+
+        return { cart, totalAmount };
+    }
 }
